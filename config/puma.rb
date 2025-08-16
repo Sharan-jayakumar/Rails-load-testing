165df0201ea5faa29013078bcfa9690eb18a49ce
Sharan Jayakumar <sharan.jayakumar@mallow-tech.com>
2025-08-16 19:30:19 +0530

    config cluster mode for task CRUD api with db integration

--- config/puma.rb
+++ config/puma.rb
@@ -14,14 +14,26 @@
 # Global VM Lock (GVL) it has diminishing returns and will degrade the
 # response time (latency) of the application.
 #
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
+# For load testing scenarios, we use cluster mode with multiple worker processes
+# to handle concurrent requests more efficiently.
 #
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+
+# Configure cluster mode for load testing
+workers ENV.fetch("WEB_CONCURRENCY", 2)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 2)
 threads threads_count, threads_count
+
+# Preload the application for better performance in cluster mode
+preload_app!
+
+# Worker timeout for graceful shutdown
+worker_timeout 3600
+
+# Worker boot timeout
+worker_boot_timeout 60
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
